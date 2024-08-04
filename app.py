00e3ba79a8f39e52889efc2581cab2d644c8fed8
manheraa <manuheralagi4@gmail.com>
2024-08-04 19:33:43 +0530

    req

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ hist=ConversationBufferMemory(chat_memory=sthistory)
 chain=LLMChain(llm=llm,prompt=prompt,memory=hist)
 
 import streamlit as st
-st.title("HI THIS IS GROQ INFERENCE METHOD HI KUTTI")
+st.title("HI THIS IS GROQ INFERENCE METHOD HI KUTTHI")
 for msg in sthistory.messages:
     st.chat_message(msg.type).write(msg.content)
 if x := st.chat_input():
