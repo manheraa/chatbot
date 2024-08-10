5d1655243e128edcab446c9f8c558a1d2559fc36
manheraa <manuheralagi4@gmail.com>
2024-08-10 11:47:52 +0530

    R

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ groq_api_key=os.environ['GROQ_API_KEY']
 llm=ChatGroq(model="mixtral-8x7b-32768",groq_api_key=groq_api_key)
 
 prompt_template="""
-Use the following piece of context and chat History to answer the question asked.
-Please try to provide the answer only based on the context and in few words
+Greet  and answer the question in step by step
 Question:{question}
 
 Helpful Answers:
@@ -20,7 +19,7 @@ hist=ConversationBufferMemory(chat_memory=sthistory)
 chain=LLMChain(llm=llm,prompt=prompt,memory=hist)
 
 import streamlit as st
-st.title("HI THIS IS GROQ INFERENCE METHOD HI Rohitya")
+st.title("HI ")
 for msg in sthistory.messages:
     st.chat_message(msg.type).write(msg.content)
 if x := st.chat_input():
